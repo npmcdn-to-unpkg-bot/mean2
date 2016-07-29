d3fd25bdaa7d393ccc44ac7860661be1ad378395
bhavik3184 <bhavik3184@gmail.com>
2016-07-30 00:05:59 +0530

    Rounting - Completed

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import {MessagesComponent} from "./messages/messages.component";
+import {Routes,ROUTER_DIRECTIVES} from "@angular/router";
+import {AuthenticationComponent} from "./auth/authentication.component";
 
 @Component({
     selector: 'my-app',
     styleUrls: ['js/app/app.component.css'],
     templateUrl: 'js/app/app.template.html',
-    directives:[MessagesComponent]
+    directives:[MessagesComponent,ROUTER_DIRECTIVES]
 })
+@Routes([
+    {path:'/',component:MessagesComponent},
+    {path:'/auth',component:AuthenticationComponent}
+])
 export class AppComponent {
 
 }
--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -1,8 +1,9 @@
 ///<reference path="../../typings.d.ts"/>
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import {MessageServiceComponent} from './messages/message-service.component'
+import {ROUTER_PROVIDERS} from '@angular/router'
 
 //noinspection TypeScriptCheckImport
 import { AppComponent } from "./app.component";
 
-bootstrap(AppComponent,[MessageServiceComponent]);
+bootstrap(AppComponent,[MessageServiceComponent,ROUTER_PROVIDERS]);

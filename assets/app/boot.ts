3864810781abbac4c77eb979162c47f1cb3e223b
bhavik3184 <bhavik3184@gmail.com>
2016-07-30 09:11:11 +0530

    Frontend & Backend based routing

--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -1,9 +1,11 @@
 ///<reference path="../../typings.d.ts"/>
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import {MessageServiceComponent} from './messages/message-service.component'
+import {MessageServiceComponent} from './messages/message-service.component';
+import {LocationStrategy,HashLocationStrategy} from '@angular/common';
 import {ROUTER_PROVIDERS} from '@angular/router'
+import {provide} from '@angular/core'
 
 //noinspection TypeScriptCheckImport
 import { AppComponent } from "./app.component";
 
-bootstrap(AppComponent,[MessageServiceComponent,ROUTER_PROVIDERS]);
+bootstrap(AppComponent,[MessageServiceComponent,ROUTER_PROVIDERS,provide(LocationStrategy,{useClass:HashLocationStrategy})]);

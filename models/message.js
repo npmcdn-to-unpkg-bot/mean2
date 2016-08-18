dffe80db8c01b6eff95686a921a5d16c52a9777d
bhavik3184 <bhavik3184@gmail.com>
2016-08-18 08:29:04 +0530

    Using moongose middleware so that we can cascade deleting of messages from both Message model & User Model. This is kind of Trigger in SQL where upon successfully deleting a message in message model will also remove the reference from user model

--- models/message.js
+++ models/message.js
@@ -1,9 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+var User = require('../models/user');
 var schema = new Schema({
     content: {type: String, required: true},
     user: {type: Schema.Types.ObjectId, ref: 'User'}
 });
 
+schema.post('remove',function(doc){
+    var deletedMsg = doc;
+    User.findById(doc.user,function(err,doc){
+        doc.messages.pull(deletedMsg);
+        doc.save();
+    });
+});
 module.exports = mongoose.model('Message', schema);
